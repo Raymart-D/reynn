8e58fe586a145cdd58060dead7b9386923f90aa0
Raymart De Guzman <deguetaraymart1730@gmail.com>
2025-04-20 13:33:38 +0000

    Keep up Raymart Pogi malapit na sumakses

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Dashboard from './pages/Dashboard';
 import RecordsManagement from './pages/RecordsManagement';
 import Reports from './pages/Reports';
 import Settings from './pages/Settings';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 
 const App: React.FC = () => {
     return (
@@ -15,6 +17,8 @@ const App: React.FC = () => {
                 <Route path="/dashboard/records-management" element={<RecordsManagement />} />
                 <Route path="/dashboard/reports" element={<Reports />} />
                 <Route path="/dashboard/settings" element={<Settings />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
                 <Route path="*" element={<div>Page Not Found</div>} />
             </Routes>
         </Router>
